a92eb8cf050874a3347d4d8b4e84b9e0713e4d46
rodes nzie <rodesnzie@gmail.com>
2025-05-25 03:30:45 +0100

    add vercel.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AboutView from '../views/ListJobs.vue'
 import HomeView from '@/views/HomeView.vue'
 import LisCompanies from '@/views/LisCompanies.vue'
 import ListFreelancers from '@/views/ListFreelancers.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
